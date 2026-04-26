a2de13a89f68a7e8a7e498e71f64269a30c00a62
vincevella10 <vellav1@my.smccd.edu>
2026-04-26 16:12:41 -0700

    Updated the layout of the GUI.

--- GUI/src/App.java
+++ GUI/src/App.java
@@ -5,33 +5,37 @@ import javax.swing.*;
 
 public class App extends JFrame {
     public static void main(String[] args) throws Exception {
+
         JFrame mainFrame = new JFrame();
         mainFrame.setTitle("Welcome");
         mainFrame.setSize(400, 300);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        mainFrame.setLayout(new GridLayout(3, 1, 10, 10)); // (rows, cols, hgap, vgap)
+        mainFrame.setLayout(new GridLayout(5, 4, 10, 10)); // (rows, cols, hgap, vgap)
+        
 
         JPanel topPanel = new JPanel();
-        JButton GenerateButton = new JButton ("generate");
+        JButton GenerateButton = new JButton ("generate Sentence");
         topPanel.add(GenerateButton);
         mainFrame.add(topPanel);
+
 
         JPanel topPanel2 = new JPanel();
         JButton GenerateButton2 = new JButton ("Add");
         topPanel2.add(GenerateButton2);
         mainFrame.add(topPanel2);
+        
 
 
         JPanel centralPanel = new JPanel();
-        JTextField WTypePanel = new JTextField(10);
+        JTextField WTypePanel = new JTextField(20);
         centralPanel.add(WTypePanel);
         mainFrame.add(centralPanel);
 
-        JPanel bottomPanel = new JPanel();
-        JTextField Instructions = new JTextField("Enter Your New Word in the Text Box to the Left", 30);
-        bottomPanel.add(Instructions);
-        mainFrame.add(bottomPanel);
+        JPanel bottomishPanel = new JPanel();
+        JLabel Instructions = new JLabel("Enter Your New Word in the Text Box above and Click Add");
+        bottomishPanel.add(Instructions);
+        mainFrame.add(bottomishPanel);
 
 
         GenerateButton2.addActionListener(new ActionListener() {
@@ -43,7 +47,7 @@ public class App extends JFrame {
     });
 
       mainFrame.setVisible(true); // this must be the last statement
-   }
+    }
 
 }
 
