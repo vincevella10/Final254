d8c90df08a3fd8d7d755d4bdbae4cc134206c5e4
vincevella10 <vellav1@my.smccd.edu>
2026-04-28 09:56:15 -0700

    Changed ReadMe and fixed GUI

---
+++ GUI/src/App.java
@@ -0,0 +1,82 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class App extends JFrame {
+    public static void main(String[] args) throws Exception {
+        JFrame mainFrame = new JFrame();
+        mainFrame.setTitle("Welcome");
+        mainFrame.setSize(400, 300);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        mainFrame.setLayout(new GridLayout(5, 5, 10, 10)); // (rows, cols, hgap, vgap)
+
+        JPanel topPanel = new JPanel();
+        JButton GenerateButton = new JButton ("generate");
+        topPanel.add(GenerateButton);
+        mainFrame.add(topPanel);
+
+        JPanel sentencePanel = new JPanel();
+        JLabel sentenceLabel = new JLabel("Your Sentence will appear here");
+        sentencePanel.add(sentenceLabel);
+        mainFrame.add(sentencePanel);
+
+        JPanel topPanel2 = new JPanel();
+        JButton GenerateButton2 = new JButton ("Add");
+        topPanel2.add(GenerateButton2);
+        mainFrame.add(topPanel2);
+
+        JButton GenerateButton3 = new JButton ("Remove");
+        topPanel2.add(GenerateButton3);
+
+
+        JPanel centralPanel = new JPanel();
+        JTextField WTypePanel = new JTextField(10);
+        centralPanel.add(WTypePanel);
+        mainFrame.add(centralPanel);
+
+        JPanel bottomPanel = new JPanel();
+        JLabel Instructions = new JLabel("Enter Your New Word or word you want to remove in the Text Box to the above");
+        bottomPanel.add(Instructions);
+        mainFrame.add(bottomPanel);
+
+
+        String[] choices = { "Verb","Adjective", "Noun",};
+
+            final JComboBox<String> cb = new JComboBox<String>(choices);
+
+            cb.setVisible(true);
+            topPanel2.add(cb);
+
+
+        GenerateButton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            String selectedChoice = (String) cb.getSelectedItem();
+            String newWord = WTypePanel.getText(); // gets the text from the TextField component
+            Instructions.setText(newWord + " (" + selectedChoice + ") has been added"); // updates the message on the label
+        }
+    });
+
+    GenerateButton3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            String selectedChoice = (String) cb.getSelectedItem();
+            String newWord = WTypePanel.getText(); // gets the text from the TextField component
+            Instructions.setText(newWord + " (" + selectedChoice + ") has been removed"); // updates the message on the label
+            }
+        });
+    GenerateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            String newSentence = "";
+            sentenceLabel.setText("Your Sentence is: " + newSentence); // updates the message on the label
+        }
+    });
+
+      mainFrame.setVisible(true); // this must be the last statement
+    }
+
+}
+
