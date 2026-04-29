d0fbf7a1832ec7f576a73b8a66b4352a520ee721
vincevella10 <vellav1@my.smccd.edu>
2026-04-29 09:11:29 -0700

    Changed so wording and fixed the formate.

--- GUI/src/App.java
+++ GUI/src/App.java
@@ -3,12 +3,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
+
 public class App extends JFrame {
     public static void main(String[] args) throws Exception {
         JFrame mainFrame = new JFrame();
         mainFrame.setTitle("Welcome");
         mainFrame.setSize(400, 300);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Color lightBlue = new Color(173, 216, 230);
+        mainFrame.setBackground(lightBlue);
+   
 
         mainFrame.setLayout(new GridLayout(5, 5, 10, 10)); // (rows, cols, hgap, vgap)
 
@@ -37,17 +41,17 @@ public class App extends JFrame {
         mainFrame.add(centralPanel);
 
         JPanel bottomPanel = new JPanel();
-        JLabel Instructions = new JLabel("Enter Your New Word or word you want to remove in the Text Box to the above");
+        JLabel Instructions = new JLabel("Enter your new word or a word you want to remove in the Text Box to the above");
         bottomPanel.add(Instructions);
         mainFrame.add(bottomPanel);
 
 
         String[] choices = { "Verb","Adjective", "Noun",};
 
-            final JComboBox<String> cb = new JComboBox<String>(choices);
+        final JComboBox<String> cb = new JComboBox<String>(choices);
 
-            cb.setVisible(true);
-            topPanel2.add(cb);
+        cb.setVisible(true);
+        topPanel2.add(cb);
 
 
         GenerateButton2.addActionListener(new ActionListener() {
@@ -74,9 +78,13 @@ public class App extends JFrame {
             sentenceLabel.setText("Your Sentence is: " + newSentence); // updates the message on the label
         }
     });
-
-      mainFrame.setVisible(true); // this must be the last statement
+        mainFrame.getContentPane().setBackground(lightBlue);
+        topPanel.setBackground(lightBlue);
+        topPanel2.setBackground(lightBlue);
+        centralPanel.setBackground(lightBlue);
+        bottomPanel.setBackground(lightBlue);
+        sentencePanel.setBackground(lightBlue);
+        mainFrame.setVisible(true); // this must be the last statement
     }
 
 }
-
